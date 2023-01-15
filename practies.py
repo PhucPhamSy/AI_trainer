def96cdd555ca7deda69e179d2b0d9c6977150a7
sppham <phamsyphuc123@gmail.com>
2023-01-16 02:30:26 +0900

    Update practies.py

--- practies.py
+++ practies.py
@@ -54,6 +54,7 @@ class poseDetector():
 
 
         if draw:
+            # draw angle in body
             cv2.circle(img, (x1, y1), 20 ,(255,0,0), cv2.FILLED)
             cv2.circle(img, (x1, y1), 25, (255, 0, 0), 2)
             cv2.line(img,(x1,y1),(x2,y2),(255,255,255),15)
